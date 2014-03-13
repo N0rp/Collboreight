8b6c6161f0a4377a1e068c32804fda80828ed156
Norp <assembla2.20.lumiken@spamgourmet.com>
2014-03-13 16:37:18 +0100

    initial commit

---
+++ src/com/example/collaboreight/MyView.java
@@ -0,0 +1,136 @@
+package com.example.collaboreight;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
+
+public class MyView extends View {
+
+    private static final float MINP = 0.25f;
+    private static final float MAXP = 0.75f;
+
+    
+    /** Contains the path the user has touched **/
+    private ScaledPath    mPath;
+
+	private float mX, mY;
+	private static final float TOUCH_TOLERANCE = 4;
+   
+	
+	private ScaleGestureDetector mScaleDetector;
+	
+	private Painting painting;
+
+    public MyView(Context c, Paint mPaint) {
+        super(c);
+        painting = new Painting(mPaint);
+        mPath = new ScaledPath();
+        
+
+        mScaleDetector = new ScaleGestureDetector(c, new ScaleListener());
+    }
+
+    public void setScale(float scale){
+    	painting.setScale(this, scale);
+    }
+
+
+    @Override
+   protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        Log.d(this.getClass()+"", "OnSizeChange to "+w+":"+h);
+        painting.onSizeChanged(w, h, oldw, oldh);
+    }
+    
+    
+    private boolean isScaleGesture = false;
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+    	if(mScaleDetector.isInProgress()){
+    		painting.draw(canvas, null);
+    	}else{
+    		painting.draw(canvas, mPath);
+    	}
+    	
+    }
+
+    private void touch_start(float x, float y) {
+        mPath.reset();
+        mPath.setScale(painting.getScale());
+        mPath.moveTo(x, y);
+        mX = x;
+        mY = y;
+    }
+    
+    private void touch_move(float x, float y) {
+        float dx = Math.abs(x - mX);
+        float dy = Math.abs(y - mY);
+        if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+            mPath.quadTo(mX, mY, (x + mX)/2, (y + mY)/2);
+            mX = x;
+            mY = y;
+        }
+//        invalidate();
+    }
+    
+    
+    private void touch_up() {
+    	
+    	if(isScaleGesture){	        
+    		isScaleGesture = false;
+    	}else{
+	        mPath.lineTo(mX, mY);
+	        // commit the path to our final painting
+	        painting.commitPath(mPath);
+    	}
+    	// kill this so we don't double draw
+        mPath.reset();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+
+        mScaleDetector.onTouchEvent(event);
+        
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                touch_start(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                touch_move(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                touch_up();
+           //   Mx1=(int) event.getX();
+             //  My1= (int) event.getY();
+               invalidate();
+                break;
+        }
+        return true;
+    }
+    
+
+
+	private class ScaleListener 
+	        extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+	    @Override
+	    public boolean onScale(ScaleGestureDetector detector) {
+	    	float temp = painting.getScale() * detector.getScaleFactor();
+	    	//Don't let the object get too small or too large.
+	        temp = Math.max(0.1f, Math.min(temp, 5.0f));
+	    	
+	        painting.setScale(MyView.this, temp);
+	        isScaleGesture = true;
+	        return true;
+	    }
+	}
+}
---
+++ src/com/example/collaboreight/ScaledPath.java
@@ -0,0 +1,111 @@
+package com.example.collaboreight;
+
+import android.graphics.Matrix;
+import android.graphics.Path;
+import android.graphics.RectF;
+
+public class ScaledPath extends Path {
+
+	private float scale = 1.0f;
+	
+	private double offsetX = 0;
+	private double offsetY = 0;
+	
+	private String name = null;
+	
+	/**
+	 * Create a new scaled path.
+	 */
+	public ScaledPath(){
+		super();
+	}
+	
+	
+	/**
+	 * Copy a scaled path.
+	 * @param path
+	 */
+	public ScaledPath(ScaledPath path, String name){
+		super(path);
+		this.scale = path.getScale();
+		this.offsetX = path.getOffsetX();
+		this.offsetY = path.getOffsetY();
+		this.name = name;
+	}
+    
+    public Path getCurrentScaled(float scale){
+    	Path scaled = new ScaledPath();
+    	// computed scaled path
+    	Matrix scaleMatrix = new Matrix();
+    	scaleMatrix.setScale(1/scale, 1/scale, 0, 0);
+    	// get scaled path through transform
+    	transform(scaleMatrix, scaled);
+    	return scaled;
+    }
+
+	@Override
+	public String toString(){
+		RectF rect = new RectF();
+    	computeBounds(rect, true);
+    	
+		return "ScaledPath "+name+" with area "+rect;
+	}
+	
+	/**
+	 * Set the offsite of the path when created.
+	 * 
+	 * @param x
+	 * @param y
+	 */
+	public void setOffset(double x, double y){
+		this.offsetX = x;
+		this.offsetY = y;
+	}
+	
+	public double getOffsetX(){
+		return this.offsetX;
+	}
+	
+	public double getOffsetY(){
+		return this.offsetY;
+	}
+	
+	/**
+	 * Set the scale of the current path.
+	 * @param scale
+	 */
+	public void setScale(float scale){
+		this.scale = scale;
+	}
+	
+	public float getScale(){
+		return this.scale;
+	}
+	
+	@Override
+	public void lineTo(float x, float y){
+		x /= scale;
+		y /= scale;
+		
+		super.lineTo(x, y);
+	}
+	
+	@Override
+	public void moveTo(float x, float y){
+		x /= scale;
+		y /= scale;
+		
+		super.moveTo(x, y);
+	}
+	
+	@Override
+	public void quadTo(float x1, float y1, float x2, float y2){
+		x1 /= scale;
+		y1 /= scale;
+		x2 /= scale;
+		y2 /= scale;
+		
+		super.quadTo(x1, y1, x2, y2);
+	}
+	
+}
